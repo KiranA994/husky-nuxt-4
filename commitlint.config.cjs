4b5502c057b10748fcb82112eab3d5702b052fed
KiranA994 <kiranagrasala13@gmail.com>
2026-01-03 10:37:09 +0530

    feat(husky): husky added

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -25,7 +25,7 @@ module.exports = {
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'header-max-length': [2, 'always', 20],
+    'header-max-length': [2, 'always', 72],
 
     /* Optional */
     'scope-case': [2, 'always', 'kebab-case'],
